4cf91a7f419c549355aedb278156861c43b17dd9
sahalmahin <samirsatter0142@gmail.com>
2023-12-03 19:25:44 +0600

    user url updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
                             <>
                                 <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                                     <div className=" rounded-full border border-gray-400">
-                                        <img alt="" src='../../images/user.png' />
+                                        <img alt="" src='https://i.postimg.cc/nVqxVd0Y/user.png' />
                                     </div>
                                 </label>
                                 <Link to='/login'><button className="btn btn-outline bg-white ml-1">Login</button></Link>
